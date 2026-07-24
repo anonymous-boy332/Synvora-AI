d9679aa90853862de386e73720c8656b89096e26
Atiq Ur Rehman <anonymousboy7890@gmail.com>
2026-07-24 20:33:26 +0500

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,75 @@
+export default async function handler(req, res) {
+
+if(req.method!=="POST"){
+
+return res.status(405).json({
+
+error:"Method not allowed"
+
+});
+
+}
+
+const {message}=req.body;
+
+try{
+
+const response=await fetch("https://api.groq.com/openai/v1/chat/completions",{
+
+method:"POST",
+
+headers:{
+
+"Authorization":`Bearer ${process.env.GROQ_API_KEY}`,
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify({
+
+model:"llama-3.1-8b-instant",
+
+messages:[
+
+{
+
+role:"system",
+
+content:"You are Synvora AI. You help users with business, coding, AI, marketing and productivity."
+
+},
+
+{
+
+role:"user",
+
+content:message
+
+}
+
+]
+
+})
+
+});
+
+const data=await response.json();
+
+return res.status(200).json({
+
+reply:data.choices[0].message.content
+
+});
+
+}catch(err){
+
+return res.status(500).json({
+
+error:err.message
+
+});
+
+}
+
+}
