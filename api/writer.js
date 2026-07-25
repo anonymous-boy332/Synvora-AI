5e1c2654f246b1d587cef4b99e7cb1c0f1943d60
Atiq Ur Rehman <anonymousboy7890@gmail.com>
2026-07-25 06:00:13 +0500

    Update writer.js

--- api/writer.js
+++ api/writer.js
@@ -1,5 +1,7 @@
 export default async function handler(req, res) {
 
+  console.log("Writer API Loaded");
+
     if (req.method !== "POST") {
         return res.status(405).json({
             error: "Method not allowed"
